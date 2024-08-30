8223cb8e67be74aa6413e045325343b9e87ca54f
Oleksandr_Zhyvohliad~ <aleks-zhiv@ukr.net>
2024-08-30 22:17:59 +0300

    upd_fin

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = loginForm.elements.email;
+    const password = loginForm.elements.password;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
+    if (!emailValue || !passwordValue) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+});
